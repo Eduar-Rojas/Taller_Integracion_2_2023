70bc4ecc94eb92e462c071cb3cddabcac10b255e
coto24 <jdelpino2020@alu.uct.cl>
2023-10-09 18:36:24 -0300

    Avance de catálogo
    Se realizó el catálogo y se dejó en una página aparte en el archivo catalogo.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Carro from './Components/Carro.jsx'
+import Catalogo from './pages/Catalogo.jsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< Updated upstream
     <App/>
-=======
-    <Carro />
-
->>>>>>> Stashed changes
   </React.StrictMode>,
 )
