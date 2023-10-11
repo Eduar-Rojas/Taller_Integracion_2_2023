34eda74028c6ba426d5e05779b970a8661827b0d
JaviCuri <jcuripan2022@alu.uct.cl>
2023-10-11 11:58:45 -0300

    Auto stash before merge of "feature" and "origin/feature"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+<<<<<<< Updated upstream
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App/>
+=======
+import { LoginRegister } from './pages/LoginRegister.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoginRegister />
+
+>>>>>>> Stashed changes
   </React.StrictMode>,
 )
