0d3411cc77cff27ffa8a1da301cb8ab6934df139
Catalinafonsec4 <cfonseca2022@alu.uct.cl>
2023-10-15 13:57:27 -0300

    Mensaje alerta en Register/login (codigo comentado)

--- frontend/src/pages/LoginRegister.jsx
+++ frontend/src/pages/LoginRegister.jsx
@@ -6,38 +6,103 @@ import password_icon from '../Components/assets/img/contraseña.png'
 import logo from '../Components/assets/img/Logo.png'
 import { Link } from 'react-router-dom';
 import Axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import validator from 'validator';
+
+ 
+
 
 export const LoginRegister = () => {
 
-    const [action, setAction] = useState("Registrar");
+  
+    const[action,setAction] = useState("Registrar");
 
 
-    const [formulario, obtenerformulario] = useState({
-        nombre: '',
-        correo: '',
-        contrasena: '',
-    })
+    //useState :administra  estados 
+/*  formulario:variable de estado,dentro de el estan 
+      *nombre
+      *correo
+      *contrasena
+      esto son campos de entrda y se inicializan en cero
+
+*/
+    
+//obtnerfomulario :la funcion que permite actualizar a formulario
+const [formulario,obtenerformulario]=useState({
+    user:'',
+    email:'',
+    pass:'',
+})
 
 
-    const btoninput = (e) => {
-        const { name, value } = e.target;
-        obtenerformulario({
-            ...formulario,
-            [name]: value,
-        });
-    };
+
+/*la funcion btoninput hace que cunado el usuario ingresa sus datos,va a estar actualizando el valor del campo
+ cuando se dispara esta funcion en el input del formulario,con e.target se extraen la propiedad name(se identifica
+   el campo con un nombre) y el value(el valor actual del campo) */
+const btoninput=(e)=>{
+    const{name,value}=e.target;
+    obtenerformulario({//aqui se usa la funcion obtnerfomrulario para actualizar el estado formulario
+        ...formulario,// ...formulario  ,eso significa  que  toma  todos los campos de entrada 
+        [name]:value,
+    });
+};
+
 
 
     const enviar = async (e) => {
         e.preventDefault();
+        //mensaje de  alerta para que cuando se registre  correo sea correcto 
+    if (!validator.isEmail(formulario.email)) {
+        toast.error('Introduzca un correo electrónico válido', {
+          position: 'top-right',
+          autoClose: 5000,
+        });
+        return;
+      }
+        //validar nombre si esta vacio 
+    if (formulario.user.trim()===""  ){
+        toast.error('Nombre esta vacio ', {
+            position: 'top-right',
+            autoClose: 5000,
+        });
+    return
+    }
+        // validar nombre  que  ademas sea solo letras ,no se permiten numeros
+    if (!/^[A-Za-z]+$/.test(formulario.user)) {
+        toast.error('Introduzca un nombre válido (solo letras)', {
+            position: 'top-right',
+            autoClose: 5000,
+        });
+        return;
+    }
 
-        try {
-            const respuesta = await Axios.post('http://localhost:3000/registro', formulario);
-            if (respuesta.status === 200) {
-                console.log('registro exitoso');
+    //validar contraseña
+    if(formulario.pass.length<5){
+        toast.error('Introduzca  una contraseña minimo 5 caracteres',{ 
+        position: 'top-right',
+        autoClose:5000,
+    });
+    return
+ }
+
+ 
+    try{
+      // se hace una solicitud post a dicha url  con los datos del formulario
+        const respuesta=await Axios.post('http://localhost:3000/registro',formulario);
+        if( respuesta.status===200){//respuesta de estado del servidor
+            toast.success('Registro exitoso', {//mensaje de alerta en la parte superior derecha 
+                position: 'top-right', 
+                autoClose: 5000, // dura 5segundos
+              });
             }
-        } catch (error) {
-            console.error('error en el registro', error);
+       
+    }catch(error){
+        toast.error('Error en el registro', {
+            position: 'top-right',
+            autoClose: 5000,
+          });
+        console.error('error en el registro',error);
 
         }
     }
@@ -46,57 +111,83 @@ export const LoginRegister = () => {
         e.preventDefault();
 
         try {
-            const respuesta = await Axios.post('http://localhost:3000/inicio-sesion', formulario);
+            const respuesta = await Axios.post('http://localhost:3000/login', formulario);
             if (respuesta.status === 200) {
-                console.log('Inicio de sesión exitoso');
-                // Aquí podrías redirigir al usuario a otra página después del inicio de sesión exitoso
+                toast.success('Inicio de sesion exitoso', {//mensaje de alerta en la parte superior derecha 
+                    position: 'top-right', 
+                    autoClose: 5000, // dura 5segundos
+                  });
+                  console.log('Inicio de sesión exitoso');
+                  // Aquí podrías redirigir al usuario a otra página después del inicio de sesión exitoso
+                
+            
             }
         } catch (error) {
+
+            toast.success('Error en el inicio de sesión', {//mensaje de alerta en la parte superior derecha 
+                position: 'top-right', 
+                autoClose: 5000, // dura 5segundos
+              });
+
             console.error('Error en el inicio de sesión', error);
         }
     };
 
 
+  return (
+    <div className="outer-container">
+    <div className="logo-container">
+        <img src={logo} alt="" className='logo' />
+    </div>
+    <div className='container'>
 
-    return (
-        <div className="outer-container">
-            <div className="logo-container">
-                <img src={logo} alt="" className='logo' />
-            </div>
-            <div className='container'>
-
-                <div className='header'>
-                    <Link to="/">
-                        <button className=' font-bold bg-white text-black w-36 h-10 rounded-3xl hover:bg-[#49424e] hover:text-white'>
-                            VOLVER
-                        </button>
-                    </Link>
-                    <div className="text">{action}</div>
-                    <div className="underline"></div>
-                </div>
-                <form onSubmit={action === 'Registar' ? enviar : enviarSesion} >
-                    <div className="inputs">
-                        {action === "Iniciar Sesión" ? <div></div> : <div className="input">
-                            <img src={user_icon} alt="" />
-                            <input type="text" placeholder='Nombre' name='nombre' value={formulario.nombre} onChange={btoninput} required />
-                        </div>}
-
-                        <div className="input">
-                            <img src={email_icon} alt="" />
-                            <input type="email" placeholder='Correo electrónico' name='correo' value={formulario.correo} onChange={btoninput} required />
-                        </div>
-                        <div className="input">
-                            <img src={password_icon} alt="" />
-                            <input type="password" placeholder='Contraseña' name='contrasena' value={formulario.contrasena} onChange={btoninput} required />
-                        </div>
-                        {action === "Registrar" ? <div></div> : <div className="forgot-password"><strong>¿Perdiste tu contraseña?</strong> <span>Presiona aquí</span></div>}
-                        <div className="submit-container">
-                            <button type="submit" className={action === 'Iniciar Sesión' ? 'submit active' : 'submit'} onClick={(e) => { setAction('Registrar'); enviar(e); }}>Registrar</button>
-                            <button className={action === 'Registrar' ? 'submit active' : 'submit'} onClick={(e) => { setAction('Iniciar Sesión'); enviarSesion(e); }}>Iniciar Sesión</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+        <div className='header'>
+            <Link to="/">
+                <button className=' font-bold bg-white text-black w-36 h-10 rounded-3xl hover:bg-[#49424e] hover:text-white'>
+                    VOLVER
+                </button>
+            </Link>
+            <div className="text">{action}</div>
+            <div className="underline"></div>
         </div>
-    )
+        <form onSubmit={action === 'Registar' ? enviar : enviarSesion} >
+            <div className="inputs">
+                {action === "Iniciar Sesión" ? <div></div> : <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" placeholder='Nombre' name='user' value={formulario.user} onChange={btoninput} required />
+                </div>}
+
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" placeholder='Correo electrónico' name='email' value={formulario.email} onChange={btoninput} required />
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" placeholder='Contraseña' name='pass' value={formulario.pass} onChange={btoninput} required />
+                </div>
+                {action === "Registrar" ? <div></div> : <div className="forgot-password"><strong>¿Perdiste tu contraseña?</strong> <span>Presiona aquí</span></div>}
+                <div className="submit-container">
+                    <button type="submit" className={action === 'Iniciar Sesión' ? 'submit active' : 'submit'} onClick={(e) => { setAction('Registrar'); enviar(e); }}>Registrar</button>
+                    <button className={action === 'Registrar' ? 'submit active' : 'submit'} onClick={(e) => { setAction('Iniciar Sesión'); enviarSesion(e); }}>Iniciar Sesión</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+      
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    
+    
+    />
+    </div>
+  )
 }
