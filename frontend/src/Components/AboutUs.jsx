662e6ed47b9c9439fd3ec1adf4658ba595f69953
coto24 <jdelpino2020@alu.uct.cl>
2023-10-26 20:33:33 -0300

    Pagina sobre nosotros estilizada y maquetada
    Se eliminó el archivo BurgerButton correspondiente al carrito antiguo.

---
+++ frontend/src/Components/AboutUs.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export const AboutUs = () => {
+  return (
+<div>
+    <div className='justify-self-center' >
+        <h1 className='md:text-4xl lg:text-6xl font-bold  flex items-center justify-center text-white p-15'>Sobre nosotros</h1>
+    </div>
+    <div className='text-white'>
+        <p className='p-4'>¡Bienvenido a Sushipleto, el lugar donde tu amor por el sushi y la personalización se fusionan en una experiencia única! Aquí, un grupo apasionado de estudiantes te brinda la oportunidad de diseñar tu propio sushi a tu manera. ¿Imaginas un rollo que refleje tus gustos y preferencias culinarias? En Sushipleto, 
+        hacemos que eso sea una realidad. Comienza seleccionando entre una amplia gama de ingredientes frescos y deliciosos, desde el más exquisito pescado hasta opciones vegetarianas y veganas llenas de sabor. La creatividad es tu aliada, y puedes personalizar cada detalle, desde la elección de ingredientes hasta el nivel de picante.
+        ¿Tienes antojo de un rollo crujiente con aguacate y tempura de camarones? ¡Claro! ¿O prefieres algo más exótico, como un rollo de aguacate y mango para un toque tropical? En Sushipleto, las posibilidades son infinitas. Además, nuestra preocupación por el medio ambiente se refleja en nuestras opciones de empaque sostenible, 
+        para que puedas disfrutar de tu sushi personalizado con la conciencia tranquila. Y no te preocupes por la espera, porque ofrecemos entregas rápidas, para que puedas disfrutar de tus creaciones de sushi donde quieras.En Sushipleto, creemos que la personalización es la clave para una experiencia culinaria única. Únete a la revolución del sushi 
+        y descubre el placer de la creación en cada bocado. Tu sushi, tu estilo, tu elección. ¡Bienvenido a Sushipleto, donde el arte del sushi se adapta a ti!</p>
+    </div>
+    <div className="collapse collapse-arrow bg-[#B1A7A6] font-bold text-black">
+        <input type="radio" name="my-accordion-2" checked="checked" /> 
+    <div className="collapse-title text-xl font-medium">
+        Preguntas frecuentes
+    </div>
+    <div className="collapse-content"> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    </div>
+    <div className="collapse collapse-arrow bg-[#B1A7A6] font-bold text-black">
+    <input type="radio" name="my-accordion-2" /> 
+    <div className="collapse-title text-xl font-medium">
+        ¿Dónde encontrarnos?
+    </div>
+    <div className="collapse-content"> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    </div>
+    <div className="collapse collapse-arrow bg-[#B1A7A6] font-bold text-black">
+    <input type="radio" name="my-accordion-2" /> 
+    <div className="collapse-title text-xl font-medium">
+        ¿Qué ofrecemos?
+    </div>
+    <div className="collapse-content"> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    </div>
+</div>
+  )
+}
+
+
