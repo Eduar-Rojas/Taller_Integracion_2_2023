17a17ef3f566836e39ee3055b4d8a2a708290797
Lalo <eduardo.rojas2020@alu.uct.cl>
2023-11-21 05:39:33 -0300

    Avance en SushiBuild.jsx (para establecer código funcionando)

--- backend/controller/SushiBuild.js
+++ backend/controller/SushiBuild.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db'); // Asegúrate de ajustar la ruta según tu estructura de archivos
+const db = require('../db/db');
 
-// Ruta para obtener datos de construcción de sushi
+// Ruta para obtener datos de la tabla Build_Ingredientes
 router.get('/SushiBuild', async (req, res) => {
   try {
     const query = `SELECT * FROM "Build_Ingredientes"`;
@@ -14,5 +14,32 @@ router.get('/SushiBuild', async (req, res) => {
     res.status(500).json({ error: 'Error interno del servidor al obtener datos de construcción de sushi' });
   }
 });
+
+// Ruta para obtener ingredientes por categoría
+router.get('/ingredientes', async (req, res) => {
+  const { categoria } = req.query;
+
+  try {
+    // Ajustamos la consulta para manejar los casos
+    let query;
+    if (categoria === 'Proteina') {
+      query = `SELECT * FROM "Build_Ingredientes" WHERE categoria ILIKE '%Proteina%'`;
+    } else if (categoria === 'Relleno') {
+      query = `SELECT * FROM "Build_Ingredientes" WHERE categoria ILIKE '%Relleno%'`;
+    } else if (categoria === 'Envoltura') {
+      query = `SELECT * FROM "Build_Ingredientes" WHERE categoria ILIKE '%Envoltura%'`;
+    } else {
+      query = `SELECT * FROM "Build_Ingredientes" WHERE $1 = ANY(string_to_array(categoria, ',')::text[])`;
+    }
+
+    const ingredientes = await db.any(query, [categoria]);
+
+    res.status(200).json(ingredientes);
+  } catch (error) {
+    console.error('Error al obtener ingredientes por categoría:', error);
+    res.status(500).json({ error: 'Error interno del servidor al obtener ingredientes por categoría' });
+  }
+});
+
 
 module.exports = router;
--- frontend/src/pages/SushiBuild.jsx
+++ frontend/src/pages/SushiBuild.jsx
@@ -4,50 +4,116 @@ import { Footer } from "../Components/Footer";
 import axios from 'axios';
 
 const SushiBuild = () => {
-  // Estados
   const [temperature, setTemperature] = useState("");
   const [wrap, setWrap] = useState("");
   const [protein, setProtein] = useState("");
   const [ingredient1, setIngredient1] = useState("");
   const [ingredient2, setIngredient2] = useState("");
+
   const [selected, setSelected] = useState(null);
-  const [selectedProtein, setSelectedProtein] = useState("");
+  const [summary, setSummary] = useState([]);
+
+  const [proteinIngredients, setProteinIngredients] = useState([]);
+  const [fillingIngredients, setFillingIngredients] = useState([]);
+  const [secondFillingIngredients, setSecondFillingIngredients] = useState([]);
+  const [wrapIngredients, setWrapIngredients] = useState([]);
 
   useEffect(() => {
-    // Realiza la solicitud al controlador para obtener los datos de construcción de sushi
-    axios.get('/api/SushiBuild')
-      .then(response => {
-        // Maneja la respuesta exitosa
-        const sushiBuildData = response.data;
-        console.log('Datos de construcción de sushi:', sushiBuildData);
-        // setSushiBuildData(sushiBuildData);
-      })
-      .catch(error => {
-        // Maneja los errores de la solicitud
-        console.error('Error al obtener datos de construcción de sushi:', error);
-      });
-  }, []); // La dependencia vacía asegura que se ejecute solo una vez al montar el componente
+    const fetchIngredients = async (categoria, setIngredientsFunction) => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/ingredientes', { params: { categoria } });
+        const ingredientsData = response.data;
+        console.log(`Datos de ${categoria}:`, ingredientsData);
+
+        // Filtra los ingredientes que contienen la categoría seleccionada
+        const filteredIngredients = ingredientsData.filter(ingredient => (
+          ingredient.categoria.includes(categoria)
+        ));
+
+        setIngredientsFunction(filteredIngredients);
+      } catch (error) {
+        console.error(`Error al obtener ingredientes de ${categoria}:`, error);
+      }
+    };
+
+    fetchIngredients('Proteina', setProteinIngredients);
+    fetchIngredients('Relleno', setFillingIngredients);
+    fetchIngredients('Relleno', setSecondFillingIngredients);
+    fetchIngredients('Envoltura', setWrapIngredients);
+  }, []);
+
+  // La dependencia vacía asegura que se ejecute solo una vez al montar el componente
 
   // Métodos
+  const updateSummary = (category, value) => {
+    // Verifica si ya hay una entrada para la categoría en el resumen
+    const existingIndex = summary.findIndex(item => item.category === category);
+
+    if (existingIndex !== -1) {
+      // Si la categoría ya existe, actualiza el valor
+      const updatedSummary = [...summary];
+      updatedSummary[existingIndex] = { category, value };
+      setSummary(updatedSummary);
+    } else {
+      // Si la categoría no existe, agrega una nueva entrada
+      const updatedSummary = [...summary, { category, value }];
+      setSummary(updatedSummary);
+    }
+  };
+
   const handleProteinChange = (event) => {
     setProtein(event.target.value);
+    updateSummary("Proteína", event.target.value);
   };
 
   const handleWrapChange = (event) => {
     setWrap(event.target.value);
-  };
-
-  const handleClick = (buttonNumber, event) => {
-    event.preventDefault();
-    setSelected(buttonNumber);
+    updateSummary("Envoltura", event.target.value);
   };
 
   const handleIngredient1Change = (event) => {
     setIngredient1(event.target.value);
+    updateSummary("Primer Relleno", event.target.value);
   };
 
   const handleIngredient2Change = (event) => {
     setIngredient2(event.target.value);
+    updateSummary("Segundo Relleno", event.target.value);
+  };
+
+  const handleClick = (buttonNumber, event) => {
+    event.preventDefault();
+    if (buttonNumber === 2) {
+      setWrap("");
+    }
+    
+    setSelected(buttonNumber);
+  };
+
+  const renderSummary = () => {
+    const summaryData = [];
+
+    // Agrega los elementos al resumen solo si están seleccionados
+    if (temperature === "1") {
+      summaryData.push({ category: "Envoltura", value: wrap });
+    }
+
+    // Agrega los elementos al resumen solo si están seleccionados
+    if (protein) {
+      summaryData.push({ category: "Proteina", value: protein });
+    }
+
+    // Agrega los elementos al resumen solo si están seleccionados
+    if (ingredient1) {
+      summaryData.push({ category: "Primer Relleno", value: ingredient1 });
+    }
+
+    // Agrega los elementos al resumen solo si están seleccionados
+    if (ingredient2) {
+      summaryData.push({ category: "Segundo Relleno", value: ingredient2 });
+    }
+
+    return summaryData;
   };
 
 
@@ -55,17 +121,17 @@ const SushiBuild = () => {
   const renderWrapSelection = () => {
     if (temperature === '1') {
       return (
-        <div>
+        <section>
           <h2 className="text-white sm:text-xl md:text-2xl lg:text-2xl">Seleccione envoltura</h2>
           <select className="select select-bordered w-full text-white mb-4" value={wrap} onChange={handleWrapChange}>
-            <option value="crema">Queso</option>
-            <option value="palta">Palta</option>
-            <option value="alga nori">Alga Nori</option>
-            <option value="salmon">Sálmon</option>
-            <option value="atun">Atún</option>
-            <option value="sesamo">Sésamo</option>
+            <option disabled selected>
+              Seleccione la envoltura
+            </option>
+            {wrapIngredients.map(ingredient => (
+              <option key={ingredient.id} value={ingredient.nombre}>{ingredient.nombre}</option>
+            ))}
           </select>
-        </div>
+        </section>
       );
     }
     return null;
@@ -116,40 +182,42 @@ const SushiBuild = () => {
 
           {renderWrapSelection()}
 
+          {/* Selección de proteína */}
           <section>
             <h2 className="text-white sm:text-xl md:text-2xl lg:text-2xl">Seleccione proteina</h2>
             <select className="select select-bordered w-full text-white mb-4" value={protein} onChange={handleProteinChange}>
               <option disabled selected>
                 Seleccione la proteina
               </option>
-              <option value="Kanikama">Kanikama</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/PolloPlancha_cz8efi.jpg">Pollo</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/Salmon_lxcldg.jpg">Salmon</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126725/Camaron_vrkrew.jpg">Camaron</option>
+              {proteinIngredients.map(ingredient => (
+                <option key={ingredient.id} value={ingredient.nombre}>{ingredient.nombre}</option>
+              ))}
             </select>
           </section>
 
+          {/* Selección de primer relleno */}
           <section>
             <h2 className="text-white sm:text-xl md:text-2xl lg:text-2xl">Seleccione primer relleno</h2>
             <select className="select select-bordered w-full text-white mb-4" value={ingredient1} onChange={handleIngredient1Change}>
               <option disabled selected>
                 Seleccione ingrediente
               </option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/QuesoCrema_ezidfu.jpg">Queso</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126725/Choclo_yvfzb8.jpg">Choclo</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/Morron_hyfxpo.jpg">Pimenton</option>
+              {fillingIngredients.map(ingredient => (
+                <option key={ingredient.id} value={ingredient.nombre}>{ingredient.nombre}</option>
+              ))}
             </select>
           </section>
-
+          
+          {/* Selección de segundo relleno */}
           <section>
             <h2 className="text-white sm:text-xl md:text-2xl lg:text-2xl">Seleccione segundo relleno</h2>
             <select className="select select-bordered w-full text-white mb-4" value={ingredient2} onChange={handleIngredient2Change}>
               <option disabled selected>
                 Seleccione ingrediente
               </option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/QuesoCrema_ezidfu.jpg">Queso</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126725/Choclo_yvfzb8.jpg">Choclo</option>
-              <option value="https://res.cloudinary.com/dx1y7g3j0/image/upload/v1698126726/Morron_hyfxpo.jpg">Pimentón</option>
+              {secondFillingIngredients.map(ingredient => (
+                <option key={ingredient.id} value={ingredient.nombre}>{ingredient.nombre}</option>
+              ))}
             </select>
           </section>
         </form>
@@ -157,8 +225,18 @@ const SushiBuild = () => {
       {/*FinDeFormulario */}
 
       <div className="row-start-3 place-self-start p-8 border-solid border-2 rounded-l-lg border-rojito border-opacity-50">
-        <h2>Aqui va el imagen al seleccionar algo + descripción</h2>
-        
+      <h2 className="sm:text-xl md:text-2xl lg:text-2xl text-white mb-4">Resumen de tu rollo sushi: </h2>
+        <ul>
+          {renderSummary().map((item, index) => (
+            <li key={index}>
+              <strong>{item.category}:</strong> {item.value}
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <button className="btn btn-xs sm:btn-sm md:btn-md">Agregar</button>
+        </div>
       </div>
 
       <div className="col-span-2 mt-24">
